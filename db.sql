2b555ecf92d21b0e0861b3107e84e3d014f26cb3
GuiLira <liraguilherme252@gmail.com>
2019-09-05 12:04:49 -0300

    Correção de erros e adição de remove_cli.

--- db.sql
+++ db.sql
@@ -17,23 +17,22 @@ CREATE TABLE clients (
 
 CREATE TABLE services (
     id INT PRIMARY KEY AUTO_INCREMENT,
-
+    id_user INT,
     client_id INT,
-
     equip VARCHAR(255),
     problem TEXT,
-
     solution TEXT,
-
     is_open BOOLEAN,
-
+    FOREIGN KEY (id_user) REFERENCES users(id),
     FOREIGN KEY (client_id) REFERENCES clients(id) ON DELETE CASCADE
 );
 
 INSERT INTO users (name, login, password)
 VALUES  ('ranieri', 'ranieri', '64834d1949af5eefa6d24a6156d39cab5c57839c'), -- senha: ranieri
-        ('o cara da loja', 'cara', '2e6f9b0d5885b6010f9167787445617f553a735f'); -- senha: teste
+        ('o cara da loja', 'cara', '2e6f9b0d5885b6010f9167787445617f553a735f'), -- senha: teste
+        ('GuileSuica', 'hehe@ha', '2e6f9b0d5885b6010f9167787445617f553a735f');
 
 INSERT INTO clients (name, cep, num, compl)
 VALUES  ('ranieri', '53415000', '000', ''),
-        ('tony', '55000000', '123', 'localizacao secreta');
+        ('tony', '55000000', '123', 'localizacao secreta'),
+        ('shubrake', '52525255', '666', 'batcaverna');
--- login.php
+++ login.php
@@ -11,7 +11,8 @@ $password = sha1($_POST['password']);
 try{
 	$stmt = $pdo->prepare('SELECT * FROM users WHERE login = ? AND password = ?');
 	$stmt->bindParam(1, $login);
-	$stmt->execute($login, $password);
+	$stmt->bindParam(2, $password);
+	$stmt->execute();
 	$res = $stmt->fetchAll();
 }catch(PDOException $e){
 	print_r($e);
@@ -19,6 +20,7 @@ try{
 
 if (sizeof($res) > 0) {
     $_SESSION['user'] = $res[0]['name'];
+    $_SESSION['id_user'] = $res['id'];
     header('location: index.php');
 } else {
     header('login_error.php');
